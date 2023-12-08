8d5f2fdf50de8aec838738bb53d72fd1e09e4269
Gustavo Henrique <ghsdare@gmail.com>
2023-12-08 20:46:55 -0300

    Exercise Js Logical
    This is my Js study repository.

--- JavaScript/002/1001.js
+++ JavaScript/002/1001.js
@@ -1,13 +1,13 @@
-/*
-Questão está no bloco de notas.
-*/
+// Importação da Biblioteca:
 const readline = require('readline');
 
+// Criação da Interface de Leitura:
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+// Pergunta ao Usuário e Processamento de Entrada:
 rl.question('Digite Valor de A: ',(inputValorA)  => {
     rl.question('Digite Valor de B: ',(inputValorB) => {
         if (!isNaN(inputValorA, inputValorB)) {
@@ -15,11 +15,13 @@ rl.question('Digite Valor de A: ',(inputValorA)  => {
             const valorB = parseInt(inputValorB);
             const soma = valorA + valorB;
             console.log(`X = ${soma}`);
+            //Fechamento da Interface:
             rl.close()
         } else {
             console.log('Não é um número');   
         }
-        rl.close();
+        // Fechamento da Interface:
+        rl.close(); 
 
     })
 })
