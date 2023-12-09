5ca8ae5c66585a4e3c49f1cc7b70632dee7b1d8c
Gustavo Henrique <ghsdare@gmail.com>
2023-12-09 12:42:44 -0300

    exercise 004

---
+++ JavaScript/004/1003.js
@@ -0,0 +1,41 @@
+// Importação de biblioteca
+const readline = require('readline');
+
+// Criação de interface
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Função para converter input para number
+const inputNumber = (input) => {
+    return parseInt(input);
+};
+
+// Pergunta / entrada de dado para input A
+rl.question('Valor A: ', (inputA) => {
+    const valorA = parseInt(inputA);
+    if (!isNaN(valorA)) {
+
+        // Pergunta / entrada de dado para input B
+        rl.question('Valor B: ', (inputB) => {
+            const valorB = parseInt(inputB);
+            if (!isNaN(valorB)) { 
+
+                // Criando soma 
+                const soma = valorA + valorB;
+                console.log(`SOMA = ${soma}`);
+                
+                // Fechar interface
+                rl.close();
+            } else {
+                console.log(`Valor B não é válido`);
+                rl.close();
+            }
+        });
+
+    } else {
+        console.log(`valor A não é válido`);
+        rl.close();
+    }
+});
